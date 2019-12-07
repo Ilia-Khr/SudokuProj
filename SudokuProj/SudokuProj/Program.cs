24a95fe0b0c10c28890a114f227d321df207cbfa
Ilia Khrenkov <14ili721@gmail.com>
2019-12-07 14:29:53 +0300

    Some sample change

--- SudokuProj/SudokuProj/Program.cs
+++ SudokuProj/SudokuProj/Program.cs
@@ -10,7 +10,8 @@ namespace SudokuProj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
         }
     }
 }
