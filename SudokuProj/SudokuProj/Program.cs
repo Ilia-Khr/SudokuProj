d551b150b58532e94f297b674e62e5f2195482bc
Дарья Мейлах <drmeylakh@edu.hse.ru>
2019-12-12 18:52:07 +0300

    just some check

--- SudokuProj/SudokuProj/Program.cs
+++ SudokuProj/SudokuProj/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SudokuProj.Manipulations;
+using SudokuProj.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,13 +12,18 @@ namespace SudokuProj
     {
         static void Main(string[] args)
         {
-            string s = Console.ReadLine();
-            Console.WriteLine(s);
-            Console.WriteLine("Howdy folks");
-            var WhenWillIlyaMarryEmmaStone = DateTime.Now;
-            Console.WriteLine($"It will happen{WhenWillIlyaMarryEmmaStone}");
-            Console.ReadLine();
+            Matrix matrix = new Matrix();
+            var randomized = new RandomizeMatrix();
 
+            for (int a = 0; a < 9; a++)
+            {
+
+                Console.WriteLine($"{randomized.finalmatrix[a][0]} {randomized.finalmatrix[a][1]} {randomized.finalmatrix[a][2]} {randomized.finalmatrix[a][3]} {randomized.finalmatrix[a][4]} {randomized.finalmatrix[a][5]} {randomized.finalmatrix[a][6]} {randomized.finalmatrix[a][7]} {randomized.finalmatrix[a][8]}");
+                Console.ReadKey();
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
