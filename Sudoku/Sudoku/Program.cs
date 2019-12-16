92ef9212bbfa05766b21888ce707eafcab20d2d7
Дарья Мейлах <drmeylakh@edu.hse.ru>
2019-12-16 19:52:56 +0300

    test

--- Sudoku/Sudoku/Program.cs
+++ Sudoku/Sudoku/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Sudoku.Manipulation;
+using Sudoku.Model;
+using System;
 
 namespace Sudoku
 {
@@ -6,7 +8,24 @@ namespace Sudoku
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Matrix matrix = new Matrix();
+            var randomized = new RandomizeMatrix();
+            HintGenerator generator = new HintGenerator(2);
+            for (int a = 0; a < 9; a++)
+            {
+
+                Console.WriteLine($"{randomized.finalmatrix[a][0]} {randomized.finalmatrix[a][1]} {randomized.finalmatrix[a][2]} {randomized.finalmatrix[a][3]} {randomized.finalmatrix[a][4]} {randomized.finalmatrix[a][5]} {randomized.finalmatrix[a][6]} {randomized.finalmatrix[a][7]} {randomized.finalmatrix[a][8]}");
+                Console.ReadKey();
+            }
+            foreach (int[] coord in generator.coordinates)
+            {
+
+                Console.WriteLine($"{randomized.finalmatrix[coord[0]][coord[1]]} at coord{coord[0]} and {coord[1]}");
+                Console.ReadKey();
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
