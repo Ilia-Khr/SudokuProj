31749ada4daa92998478708bee9306a230e75034
Андрей Потемкин <andrew_potemkin2014@mail.ru>
2019-12-18 20:20:41 +0300

    Added 1 more WPF page where the winner is congratulated and provided with choice whether to go to main menu, to play again or to visit leaderboards. All the buttons work but it's also necessary to tie up the player's time into the corresponding textblock.

---
+++ Sudoku/SudokuGame/CongratulationPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SudokuGame
+{
+    /// <summary>
+    /// Логика взаимодействия для CongratulationPage.xaml
+    /// </summary>
+    public partial class CongratulationPage : Page
+    {
+        public CongratulationPage()
+        {
+            InitializeComponent();
+        }
+
+        public void OnNewGameClick(object sender, RoutedEventArgs e)
+        {
+            Navigator.Default.Navigate(new DifficultyChooser());
+        }
+
+        public void OnLeaderboardsClick(object sender, RoutedEventArgs e)
+        {
+            Navigator.Default.Navigate(new Leaderboards());
+        }
+        private void ToMainMenu(object sender, RoutedEventArgs e)
+        {
+            Navigator.Default.Navigate(new MainMenu());
+        }
+    }
+}
